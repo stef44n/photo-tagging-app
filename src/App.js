454a6031ea04e36300a113c8466f4ca1ee0d3a6b
Stefan <stefanseifeddine@gmail.com>
2023-06-05 21:09:34 +0100

    Determine x and y coords on image click

--- src/App.js
+++ src/App.js
@@ -9,12 +9,49 @@ import fullPicture from "./assets/picture.jpg";
 
 function App() {
     function myTest(event) {
+        // let clientX = event.clientX; // Window area
+        let imageCoords = event.target.getBoundingClientRect(); //
+        // let clientY = event.clientY; // Window area (Like screen but starts from top of the web page - not screen)
+        // let offsetY = event.currentTarget.offsetY; //
+        // let screenY = event.screenY; // Screen area (Physical monitor screen - always same at cursor position)
+        let pageY = event.pageY; // Page (From top of the page all the way to the bottom of the page)
+        // let targetY = event.target.y; //
+        let targetOffTop = event.target.offsetTop; //
+        // let nativeEventY = event.nativeEvent.y; //
+        let imageYcoord = pageY - targetOffTop; //
         console.log(event);
-        console.log(event.target.x);
-        console.log(event.target.y);
-        console.log(event.nativeEvent);
-        console.log(event.nativeEvent.x);
-        console.log(event.nativeEvent.y);
+        // console.log(`event.clientX = ${clientX}`);
+        console.log(`--------------------`);
+        // console.log(`event.clientY = ${clientY}`);
+        // console.log(`event.offsetY = ${offsetY}`);
+        // console.log(`event.screenY = ${screenY}`);
+        // console.log(`event.pageY = ${pageY}`);
+        console.log(`event.pageX = ${event.pageX}`);
+        // console.log(`event.target.y = ${targetY}`);
+        // console.log(`event.target.offsetTop = ${targetOffTop}`);
+        // console.log(`event.nativeEvent.y = ${nativeEventY}`);
+        // console.log(`exact image Y-coord = ${pageY - screenY}`);
+        // console.log(`exact image Y-coord = ${pageY - targetOffTop}`); // GOOD
+        console.log(`exact image Y-coord = ${imageYcoord}`); // GOOD
+        // console.log(event.target.x);
+        // console.log(event.target.y); // Distance from target/image to top of page
+        console.log(imageCoords); // Distance from target/image to top of page
+        // console.log(event.nativeEvent);
+        // console.log(event.nativeEvent.x);
+        // console.log(event.nativeEvent.y); // Distance from top of page to cursor
+        console.log("MISSED, keep going!");
+    }
+
+    function ashFunc() {
+        console.log("Found Ash!");
+    }
+
+    function tomFunc() {
+        console.log("Found Tom!");
+    }
+
+    function jbFunc() {
+        console.log("Found JB!");
     }
 
     return (
@@ -61,14 +98,26 @@ function App() {
                 onClick={myTest}
             />
             <map name="pictureMap">
-                <area alt="ash" shape="rect" coords="64, 4218, 146, 4325" />
-                <area alt="tom" shape="rect" coords="2729, 5249, 2836, 5399" />
+                <area
+                    alt="ash"
+                    shape="rect"
+                    coords="64, 4218, 146, 4325"
+                    onClick={ashFunc}
+                />
+                <area
+                    alt="tom"
+                    shape="rect"
+                    coords="2729, 5249, 2836, 5399"
+                    onClick={tomFunc}
+                />
                 <area
                     alt="johnny-bravo"
                     shape="rect"
                     coords="1385, 3057, 1468, 3195"
+                    onClick={jbFunc}
                 />
             </map>
+
             {/* <Footer /> */}
         </div>
     );
