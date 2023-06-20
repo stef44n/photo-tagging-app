19bad48bfb4691ecfccd2bef06fb29b363265ad0
Stefan <stefanseifeddine@gmail.com>
2023-06-20 20:48:29 +0100

    Update option names

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,9 +5,9 @@ const Dropdown = ({ onSelect }) => {
     return (
         <div className="dropdown">
             <ul>
-                <li onClick={() => onSelect("Item 1")}>Character 1 - Ash</li>
-                <li onClick={() => onSelect("Item 2")}>Character 2 - Tom</li>
-                <li onClick={() => onSelect("Item 3")}>Character 3 - JB</li>
+                <li onClick={() => onSelect("Ash")}>Ash</li>
+                <li onClick={() => onSelect("Tom")}>Tom</li>
+                <li onClick={() => onSelect("Johnny Bravo")}>Johnny Bravo</li>
             </ul>
         </div>
     );
